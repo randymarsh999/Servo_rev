ab055cdd2af3b631adce6659bdf4fe075155e26d
vortexblow <vortexblow@gmail.com>
2018-06-09 10:46:56 +0300

    initial code

---
+++ servo_rev/servo_rev.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		servo_rev.ino
+ Created:	09.06.2018 10:46:38
+ Author:	root
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
