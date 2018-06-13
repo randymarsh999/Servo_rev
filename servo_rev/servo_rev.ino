1670294053df1d6e01534c657562b4ba8d1c0a47
vortexblow <vortexblow@gmail.com>
2018-06-13 12:11:06 +0300

    added average value of output voltage

--- servo_rev/servo_rev.ino
+++ servo_rev/servo_rev.ino
@@ -28,9 +28,17 @@ float Step = 5.0F / 1024; // ¬ычисл€ем шаг Uопорн / на градацию
 
 const int numReadings = 10;
 int readings[numReadings];      // the readings from the analog input
+int readingsF[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
+int readIndex1 = 0;              // the index of the current reading
+int readIndexnew = 0;              // the index of the current reading
+int readIndexnew1 = 0;
 int total = 0;                  // the running total
+int total1 = 0;                  // the running total
+int totalnew = 0;                  // the running total
+int totalnew1= 0;                  // the running total
 int average = 0;
+int average1 = 0;
 int inputPin = A5;
 
 
@@ -91,28 +99,28 @@ void StartRelayN()
 
 // функци€ считывает аналоговый вход заданное количество раз
 // и возвращает медианное отфильтрованное значение
-int readMedian(int pin, int samples) {
-	// массив дл€ хранени€ данных
-	int raw[samples];
-	// считываем вход и помещаем величину в €чейки массива
-	for (int i = 0; i < samples; i++) {
-		raw[i] = analogRead(pin);
-	}
-	// сортируем массив по возрастанию значений в €чейках
-	int temp = 0; // временна€ переменна€
-
-	for (int i = 0; i < samples; i++) {
-		for (int j = 0; j < samples - 1; j++) {
-			if (raw[j] > raw[j + 1]) {
-				temp = raw[j];
-				raw[j] = raw[j + 1];
-				raw[j + 1] = temp;
-			}
-		}
-	}
-	// возвращаем значение средней €чейки массива
-	return raw[samples / 2];
-}
+//int readMedian(int pin, int samples) {
+//	// массив дл€ хранени€ данных
+//	int raw[samples];
+//	// считываем вход и помещаем величину в €чейки массива
+//	for (int i = 0; i < samples; i++) {
+//		raw[i] = analogRead(pin);
+//	}
+//	// сортируем массив по возрастанию значений в €чейках
+//	int temp = 0; // временна€ переменна€
+//
+//	for (int i = 0; i < samples; i++) {
+//		for (int j = 0; j < samples - 1; j++) {
+//			if (raw[j] > raw[j + 1]) {
+//				temp = raw[j];
+//				raw[j] = raw[j + 1];
+//				raw[j + 1] = temp;
+//			}
+//		}
+//	}
+//	// возвращаем значение средней €чейки массива
+//	return raw[samples / 2];
+//}
 
 
 
@@ -151,6 +159,7 @@ void setup() {
 	{
 			readings[thisReading] = 0;
 	}
+	analogReference(EXTERNAL);
 
 }
 
@@ -158,46 +167,107 @@ void setup() {
 void loop() {
 	char outstr[10];
 	float f_val;
+	int in;
+
+	//in=analogRead(inputPin);
+	//total = total - readings[readIndex];
+	//readings[readIndex] = in;
+	//total = total + readings[readIndex];
+	//readIndex = readIndex + 1;
+	//if (readIndex >= numReadings) {
+	//	readIndex = 0;
+	//}
+	//average = total / numReadings;
+	//Serial.print("Avarage = ");
+	//Serial.println(average);
+
 	
-	int analogValue = analogRead(0); // «адаем переменную analogValue дл€ считывани€ показаний 
-	float voltageValue = analogValue * Step; // ѕереводим в вольты (показание * шаг) 
-	delay(500); // ∆дем пол секунды 
-
-
-
-
-
-	total = total - readings[readIndex];
-	outVal = analogRead(inputPin);
-	float f_val = (5.000 / 1024.000)*outVal;
 	
-	readings[readIndex] = analogRead(inputPin);
-	total = total + readings[readIndex];
-	readIndex = readIndex + 1;
+	in = analogRead(inputPin);
+	//total1 = total1 - readingsF[readIndex];
+	readingsF[readIndex] = in;
 	
-	if (readIndex >= numReadings) {
-		readIndex = 0;
+	//total1 = total1 + readingsF[readIndex];
+	total1 = total1 + analogRead(inputPin);
+	readIndex1 = readIndex1 + 1;
+	if (readIndex1 >= numReadings) {
+		readIndex1 = 0;
+		average1 = total1 / numReadings;
+		
+		totalnew = totalnew + total1/10;
+		++readIndexnew;
+	/*	Serial.print("total=");
+		Serial.println(total1);*/
+		//f_val = (3.34 / 1023.00)*average1;
+		//dtostrf(f_val, 7, 3, outstr);
+		/*Serial.print("avar=");
+		Serial.println(outstr);*/
+		//delay(100);
+		total1 = 0;
 	}
-	average = total / numReadings;
-	Serial.print("Avarage = ");
-	Serial.println(average);
+
+	if (readIndexnew >= numReadings)
+	{
+		totalnew = totalnew / numReadings;
+		f_val = (3.34 / 1023.00)*totalnew;
+		dtostrf(f_val, 7, 3, outstr);
+		Serial.print("avar1=");
+		Serial.println(outstr);
+		
+		totalnew1 = totalnew1 + totalnew;
+		totalnew = 0;
+		readIndexnew = 0;
+		
+		++readIndexnew1;
+	}
+
+	if (readIndexnew1 >= numReadings)
+	{
+		totalnew1 = totalnew1 / numReadings;
+		f_val = (3.34 / 1023.00)*totalnew1;
+		dtostrf(f_val, 7, 3, outstr);
+		Serial.print("avar2=");
+		Serial.println(outstr);
+		totalnew = 0;
+		readIndexnew = 0;
+		totalnew1 = 0;
+		readIndexnew1 = 0;
+	}
+
+
+
+
+
+	//total1 = total1 - readingsf[readIndex];
+	//outVal = analogRead(inputPin);
+	//f_val = (5.000 / 1024.000)*outVal;
+	//
+	//
+	//readingsF[readIndex] = analogRead(inputPin);
+	//total = total + readings[readIndex];
+	//readIndex = readIndex + 1;
+	//if (readIndex >= numReadings) {
+	//	readIndex = 0;
+	//}
+	//average = total / numReadings;
+	//Serial.print("Avarage = ");
+	//Serial.println(average);
+	//delay(100);
+	//Serial.print("Median = ");
+	////Serial.println(readMedian(inputPin, 15));
+	//delay(100);
+
 
 	
-	
-	
-	delay(100);
-	Serial.print("Median = ");
-	Serial.println(readMedian(inputPin, 15));
-	delay(100);
+	//outVal = in;
+	//
+	////delay(200); // wait 0.100
+	//Serial.println(outVal);
+	//f_val = 3.34 / 1023.00*outVal;
+	//dtostrf(f_val, 7, 3, outstr);
 
-
-	
-	outVal = analogRead(A5);
-	delay(200); // wait 0.100
-	f_val = (5.000 / 1024.000)*outVal;
-	dtostrf(f_val, 7, 3, outstr);
-	Serial.println(outstr);
-	delay(500);
+	//Serial.println(outstr);
+	//delay(500);
 				
 				
 	//full turn off
